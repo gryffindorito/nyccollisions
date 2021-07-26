30369533bf751e0c3d1d735d2afb53aee121b2f1
Ananya Shukla <38222123+gryffindorito@users.noreply.github.com>
2021-07-26 15:43:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,4 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-[theme]\n\
-base="dark"\n\
-backgroundColor="#191a1a"\n\
-secondaryBackgroundColor="#2c2b2b"\n\
-\n\
-
 " > ~/.streamlit/config.toml
